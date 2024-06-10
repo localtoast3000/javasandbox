0b726c375441a4d663ec635148de5cbe4de75e9c
Craig Chick <craig.chick.ext@up.coop>
2024-06-10 09:16:33 +0200

    Dice

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,8 @@
-import pkgs.data_structures.linked_list.*;
+import pkgs.games.Dice;
 
 public class Main {
     public static void main(String[] args){
-        LinkedList list = new LinkedList(new Node("I am the Head"));
-        list.push(new Node("I am the second element"));
-        list.push(new Node("I am the third element"));
-        list.push(new Node("I am the fourth element"));
-        list.push(new Node("I am the tail element"));
-
-        list.traverse((node) -> {
-            if(node.getIdx() == 2){
-                node.setData(123456);
-            }
-        });
-
-        System.out.println(list.getNodeAtIdx(2).getData());
+       Dice dice = new Dice(2);
+       System.out.println(dice.roll());
     }
 }
---
+++ src/pkgs/games/Dice.java
@@ -0,0 +1,24 @@
+package pkgs.games;
+
+import java.util.Random;
+
+public class Dice {
+    private Random rand = new Random();
+    private int sides;
+
+    public Dice(int sides){
+        setSides(sides);
+    }
+
+    public int roll(){
+        return rand.nextInt(sides) + 1;
+    }
+
+    private void setSides(int sides){
+        if(sides >= 2 && sides <= 12){
+            this.sides = sides;
+        } else {
+            throw new Error("Sides must be a number from 2 to 12");
+        }
+    }
+}
